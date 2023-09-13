85d70710d0a872a382c9c4a0caa1789e450b77a4
IgorKagin <60917726+IgorKagin@users.noreply.github.com>
2023-09-13 16:24:35 +0300

    Update matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -16,6 +16,22 @@ template<typename T, int N>
 struct Matrix
 {
   //constexpr const static size_t X = (std::is_same_v<int, T>? 128: 4);
-  //constexpr static const long unsigned M = getRow<T>();
+  constexpr static const long unsigned M = getRow<T>();
   T m_matrix[ M ][ M ] = { N };
 };
+
+//struct Aa
+//{
+//
+//public:
+//  enum { Rows = 3, Cols = 4 };
+//  using TRow = int[ Cols ];
+//  TRow & operator []( size_t i )
+//  {
+//    return matrix[ i ];
+//  }
+//
+//private:
+//
+//  int matrix[ Rows ][ Cols ];
+//};
