237b70b9fbce7fad3131e2e025b4c700cd79652d
IgorKagin <60917726+IgorKagin@users.noreply.github.com>
2023-09-13 16:17:07 +0300

    Create matrix.cpp

---
+++ matrix.cpp
@@ -0,0 +1,21 @@
+template<typename S>
+constexpr long unsigned getRow()
+{
+  if constexpr ( std::is_integral_v<S> )
+  {
+    return 44721;
+  }
+  else
+  {
+    return 31622; (is_floating_point_v)
+  }
+
+};
+
+template<typename T, int N>
+struct Matrix
+{
+  //constexpr const static size_t X = (std::is_same_v<int, T>? 128: 4);
+  //constexpr static const long unsigned M = getRow<T>();
+  T m_matrix[ M ][ M ] = { N };
+};
