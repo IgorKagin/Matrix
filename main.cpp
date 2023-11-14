ed4227652ea040f4e81e8d364e41dce39afd7a6d
igor <igor@igor-Inspiron-15-3525>
2023-11-14 13:01:49 +0200

    Мелкие правки.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
-#include "matrix.h"
+#include "./include/matrix.hpp"
 #include <cassert>
+
+using namespace CustomMatrix;
 
 int main( int, char** )
 {
+  {
     Matrix<int, -1> matrix;
     assert( matrix.size() == 0 ); // все ячейки свободны
     auto a = matrix[ 0 ][ 0 ];
@@ -12,12 +15,21 @@ int main( int, char** )
     assert( matrix[ 100 ][ 100 ] == 314 );
     assert( matrix.size() == 1 );
 
-    for ( auto& c : matrix )
+#ifdef DEBUG
+    std::cout << "for: "<< std::endl;
+#endif
+    for ( auto c : matrix )
     {
+#ifdef DEBUG
+    std::cout << "c: "<< std::endl;
+#endif
       int x;
       int y;
-      int v = 9;
+      int v;
       //std::tie( x, y, v ) = c;
       //std::cout << x << y << v << std::endl;
     }
+    std::tuple<int, int, int> t = matrix;
+  }
+
 }
